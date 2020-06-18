a73fc3c020139d2f3213110ffb6f909d3a8278af
Pauline Coudert <p.coudert@interface3.be>
2020-06-18 12:12:11 +0200

    ajouter print

--- condition.py
+++ condition.py
@@ -4,7 +4,7 @@ user_age= int(user_age)
 if user_age < 18:
     print ( "Désolé vous êtes trop jeune" )
 elif user_age > 130:
-    print ( "Désolé vous êtes hors limite d'age")
+    print ( "Désolé vous êtes trop vieux")
 else: 
     print(" Vous êtes le bienvenue ")
 
---
+++ hellowad.py
@@ -0,0 +1,4 @@
+print("Hello WAD!")
+
+
+print("Hello world!")
